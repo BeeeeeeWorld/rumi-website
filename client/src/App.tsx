4f32aad273dd2ff1fc96d50bdb65d1ca37df1678
BeeeeeeWorld <wangrumi666@gmail.com>
2026-03-20 18:47:54 +0800

    App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,23 +5,19 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-
+import ServicePortfolio from "./pages/ServicePortfolio";
 
 function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/service"} component={ServicePortfolio} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
     </Switch>
   );
 }
-
-// NOTE: About Theme
-// - First choose a default theme according to your design style (dark or light bg), than change color palette in index.css
-//   to keep consistent foreground/background color across components
-// - If you want to make theme switchable, pass `switchable` ThemeProvider and use `useTheme` hook
 
 function App() {
   return (
